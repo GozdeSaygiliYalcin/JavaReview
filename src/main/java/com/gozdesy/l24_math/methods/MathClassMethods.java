7aa62d3b5002d2a34b29dffd5d1dd64b94d57af4
Gozde SAYGILI YALCIN <saygiligozde@gmail.com>
2022-07-20 23:23:49 +0300

    comments are fixed

--- src/main/java/com/gozdesy/l24_math/methods/MathClassMethods.java
+++ src/main/java/com/gozdesy/l24_math/methods/MathClassMethods.java
@@ -6,7 +6,7 @@ public class MathClassMethods {
 		//temel ma
 		
 		System.out.println(Math.abs(-250)); //sayının mutlak değerini verir
-		System.out.println(Math.floor(2.3242)); //en yakın bir al tam sayıya yuvarlar
+		System.out.println(Math.floor(2.3242)); //en yakın bir alt tam sayıya yuvarlar
 		System.out.println(Math.ceil(3.4232)); //en yakın bir üst tam sayıya yuvarlar. floor ve ceil birbirinin zıttı olarak çalışıyor
 		System.out.println(Math.pow(2, 3)); // ilk girilen sayının ikinci girilen sayı üssünü hesaplıyor  ( 2^3 )
 		System.out.println(Math.random()); //random olarak 0-1 arası sayı üretir
